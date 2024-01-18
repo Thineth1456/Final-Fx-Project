1f93eae445f574a4b72bec7fcb6fa3a31aadf703
DESKTOP-2LIHI7S\user <thinethlakshan1456@gmail.com>
2024-01-18 18:32:30 +0530

    Intial Commit

---
+++ src/main/java/Controller/MainDashboardController.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+
+public class MainDashboardController {
+
+    public void userBtnOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void adminBtnOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void SuperAdminButtonOnAction(ActionEvent actionEvent) {
+
+    }
+}
